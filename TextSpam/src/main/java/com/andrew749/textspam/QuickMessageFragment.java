8587a436b091dfe2f3974e56afb2338f202e0ea3
Andrew Codispoti <andrewcod749@gmail.com>
2013-12-08 20:11:17 +0000

    autocomplete text view works!!

--- TextSpam/src/main/java/com/andrew749/textspam/QuickMessageFragment.java
+++ TextSpam/src/main/java/com/andrew749/textspam/QuickMessageFragment.java
@@ -19,28 +19,36 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.andrew749.textspam.Adapters.ContactListAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by andrew on 02/11/13.
  */
-public class QuickMessage extends Fragment {
+public class QuickMessageFragment extends Fragment {
     private static final int CONTACT_PICKER_RESULT = 1001;
     public static ArrayList<Custom> item = new ArrayList<Custom>();
     static Alerts alert;
     private static int frequency;
     private static String message;
-    EditText phonenumber_enter, frequency_enter, message_enter;
+    EditText frequency_enter, message_enter;
+    AutoCompleteTextView phonenumber_enter;
     ContactListAdapter contactListAdapter;
     Button add, contact;
     ListView lv;
+    private ArrayList<Map<String, String>> mPeopleList;
+    private SimpleAdapter mAdapter;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,15 +73,84 @@ public class QuickMessage extends Fragment {
         }, new IntentFilter("opencontact"));
     }
 
+    public void populateContacts() {
+        mPeopleList.clear();
+        Cursor people = getActivity().getContentResolver().query(
+                ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        while (people.moveToNext()) {
+            String contactName = people.getString(people
+                    .getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            String contactId = people.getString(people
+                    .getColumnIndex(ContactsContract.Contacts._ID));
+            String hasPhone = people
+                    .getString(people
+                            .getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+            if ((Integer.parseInt(hasPhone) > 0)) {
+                // You know have the number so now query it like this
+                Cursor phones = getActivity().getContentResolver().query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId,
+                        null, null);
+                while (phones.moveToNext()) {
+                    //store numbers and display a dialog letting the user select which.
+                    String phoneNumber = phones.getString(
+                            phones.getColumnIndex(
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    String numberType = phones.getString(phones.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.TYPE));
+                    Map<String, String> NamePhoneType = new HashMap<String, String>();
+                    NamePhoneType.put("Name", contactName);
+                    NamePhoneType.put("Phone", phoneNumber);
+                    if (numberType.equals("0"))
+                        NamePhoneType.put("Type", "Work");
+                    else if (numberType.equals("1"))
+                        NamePhoneType.put("Type", "Home");
+                    else if (numberType.equals("2"))
+                        NamePhoneType.put("Type", "Mobile");
+                    else
+                        NamePhoneType.put("Type", "Other");
+                    //Then add this map to the list.
+                    mPeopleList.add(NamePhoneType);
+                }
+                phones.close();
+            }
+        }
+        people.close();
+        getActivity().startManagingCursor(people);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.quickmessagefragment, container, false);
-        phonenumber_enter = (EditText) v.findViewById(R.id.numberedit);
+        phonenumber_enter = (AutoCompleteTextView) v.findViewById(R.id.numberedit);
         frequency_enter = (EditText) v.findViewById(R.id.frequencyedit);
         message_enter = (EditText) v.findViewById(R.id.messageedit);
         contact = (Button) v.findViewById(R.id.button2);
         alert = new Alerts(getActivity());
+        mPeopleList = new ArrayList<Map<String, String>>();
+        populateContacts();
+        // ContactsAutoCompleteCursorAdapter adap=new ContactsAutoCompleteCursorAdapter(getActivity(),c);
+        mAdapter = new SimpleAdapter(getActivity(), mPeopleList, R.layout.customcontactview,
+                new String[]{"Name", "Phone", "Type"}, new int[]{
+                R.id.ccontName, R.id.ccontNo, R.id.ccontType});
+        phonenumber_enter.setAdapter(mAdapter);
+        phonenumber_enter.setHint("Start typing a number");
+        phonenumber_enter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> av, View arg1, int index,
+                                    long arg3) {
+                Map<String, String> map = (Map<String, String>) av.getItemAtPosition(index);
+
+                String name = map.get("Name");
+                String number = map.get("Phone");
+                number = number.replace(" ", "");
+                phonenumber_enter.setText("" + number);
+
+            }
+        });
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
